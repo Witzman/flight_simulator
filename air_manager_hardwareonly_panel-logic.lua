c9f2dc1b7a0f71f88f113cf16b702068af2038d6
Witzman <witzman@witzman.de>
2021-10-04 21:52:43 +0200

    Update air_manager_hardwareonly_panel-logic.lua

--- air_manager_hardwareonly_panel-logic.lua
+++ air_manager_hardwareonly_panel-logic.lua
@@ -147,14 +147,6 @@ outp5 = hw_output_add("LED for Button 4", false)
 outp6 = hw_output_add("LED for Button 5", false)
 
 
-
-
---hw_Button_APApproachHold = hw_button_add("Button3", button3_pressed, button3_released)
---hw_Button_APBackcourseHold = hw_button_add("Button4", button4_pressed, button4_released)
---hw_Button_APAltLock = hw_button_add("Button5", button5_pressed, button5_released)
-
-
-
 hw_dial_heading = hw_dial_add("Autopilot heading dial", 3, new_knob_hdg)
 hw_dial_alt = hw_dial_add("Autopilot Altitude dial", 1, new_knob_alt)
 hw_Switch_APMaster = hw_switch_add("Switch APmaster", 1, Switch_APMaster_click_callback)
@@ -163,10 +155,3 @@ hw_Button_APNavLock = hw_button_add("Button2", new_ap_nav_switch_pos_fsx, button
 hw_Button_APAppr = hw_button_add("Button3", new_ap_appr_switch_pos_fsx, buttonrelease)
 hw_Button_APBc = hw_button_add("Button4", new_ap_bc_switch_pos_fsx, buttonrelease)
 hw_Button_APAlt = hw_button_add("Button5", new_ap_alt_switch_pos_fsx, buttonrelease)
---fsx_variable_subscribe("AUTOPILOT FLIGHT DIRECTOR ACTIVE", "Bool", ap_fd)
-
---fsx_variable_subscribe("AUTOPILOT ALTITUDE LOCK", "Bool", ap_altlock)
---fsx_variable_subscribe("AUTOPILOT MASTER", "Bool", ap_master)
---fsx_variable_subscribe("AUTOPILOT HEADING LOCK", "Bool", ap_headinglock)
---fsx_variable_subscribe("AUTOPILOT APPROACH HOLD", "Bool", ap_approachlock)
---fsx_variable_subscribe("AUTOPILOT BACKCOURSE HOLD", "Bool", ap_backcourselock)
